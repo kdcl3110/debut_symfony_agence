b21ffe28eee66a75b8cbb6ba0d64eca8a08bcb20
kdcl <diepeloic31@gmail.com>
2021-04-17 15:22:00 +0100

    version4: ajoyt de l'authentification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $lastUserName = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'controller_name' => 'SecurityController',
+            'last_username' => $lastUserName,
+            'error' =>  $authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+    }
+}
